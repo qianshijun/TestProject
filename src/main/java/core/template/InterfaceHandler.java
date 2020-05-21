df503b6340134be71482c925ab1540185ac06d16
qianshijun <372320119@qq.com>
2020-05-22 01:12:30 +0800

    第一次push代码

---
+++ src/main/java/core/template/InterfaceHandler.java
@@ -0,0 +1,17 @@
+package core.template;
+
+public interface InterfaceHandler {
+
+    /**
+     * 检查入参
+     */
+    default void checkParameter(){};
+
+
+    /**
+     * 服务处理
+     */
+    //Response doProcess();
+
+
+}
