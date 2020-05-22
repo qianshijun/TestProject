bef97f24d052e13b71b2652a175c75b862b5dec7
qianshijun <372320119@qq.com>
2020-05-22 16:19:28 +0800

    dev4重头再来

---
+++ src/main/java/core/template/InterfaceHandler.java
@@ -0,0 +1,29 @@
+package core.template;
+
+public interface InterfaceHandler {
+
+//我在github上添加了
+    /**
+     * 重新开始
+     */
+
+
+
+
+    /**
+    *这相当于dev-fb
+    *我自己修改了代码
+    */
+    /**
+     * 检查入参
+     */
+    default void checkParameter(){};
+
+
+    /**
+     * 服务处理
+     */
+    //Response doProcess();
+
+
+}
